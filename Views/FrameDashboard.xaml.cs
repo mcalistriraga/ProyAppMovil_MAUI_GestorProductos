9630f043b793566bf770be527b428f49ca0950b7
mcalistriraga <mcalistri.freel@gmail.com>
2025-06-13 16:56:24 -0400

    Ajustes de diseño en FrameDashboard: orden de botones y fondo azul claro

--- Views/FrameDashboard.xaml.cs
+++ Views/FrameDashboard.xaml.cs
@@ -1,6 +1,5 @@
 using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.Xaml;
-using MauiAppGestorMovil.Views;
+using MauiAppGestorMovil.ViewModels;
 
 namespace MauiAppGestorMovil.Views
 {
@@ -9,24 +8,7 @@ namespace MauiAppGestorMovil.Views
         public FrameDashboard()
         {
             InitializeComponent();
-        }
-
-        private void OnBuscarClicked(object sender, EventArgs e)
-        {
-            // Aquí puedes navegar a la vista de búsqueda o mostrar un mensaje
-            DisplayAlert("Buscar", "Función Buscar en desarrollo", "OK");
-        }
-
-        private void OnCategoriasClicked(object sender, EventArgs e)
-        {
-            // Aquí puedes navegar a la vista de categorías o mostrar un mensaje
-            DisplayAlert("Categorías", "Función Categorías en desarrollo", "OK");
-        }
-
-        private async void OnProductosClicked(object sender, EventArgs e)
-        {
-            // Navegamos a la pantalla de gestión de productos
-            await Navigation.PushAsync(new GestionDeProductos());
+            BindingContext = new FrameDashboardViewModel(); // Enlaza el ViewModel
         }
     }
 }
