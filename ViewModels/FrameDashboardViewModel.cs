54ba0372cd8f94c1f58aed96db7c3a127d94409f
mcalistriraga <mcalistri.freel@gmail.com>
2025-06-13 16:56:24 -0400

    Ajustes de diseño en FrameDashboard: orden de botones y fondo azul claro

---
+++ ViewModels/FrameDashboardViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+using System;
+
+namespace MauiAppGestorMovil.ViewModels
+{
+    public class DashboardOption
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public ICommand Command { get; set; }
+        public Color ButtonColor { get; set; } // Nueva propiedad para el color del botón
+    }
+
+    public class FrameDashboardViewModel
+    {
+        public ObservableCollection<DashboardOption> DashboardOptions { get; set; }
+
+        public FrameDashboardViewModel()
+        {
+            DashboardOptions = new ObservableCollection<DashboardOption>
+            {
+                new DashboardOption
+                {
+                    Title = "Buscar",
+                    Icon = "buscar.png",
+                    ButtonColor = Color.FromArgb("#4CAF50"),
+                    Command = new Command(() =>
+                        Application.Current.MainPage.DisplayAlert("Buscar", "Función Buscar en desarrollo", "OK"))
+                },
+                new DashboardOption
+                {
+                    Title = "Categorías",
+                    Icon = "categorias.png",
+                    ButtonColor = Color.FromArgb("#2196F3"),
+                    Command = new Command(() =>
+                        Application.Current.MainPage.DisplayAlert("Categorías", "Función Categorías en desarrollo", "OK"))
+                },
+                new DashboardOption
+                {
+                    Title = "Productos",
+                    Icon = "productos.png",
+                    ButtonColor = Color.FromArgb("#FF9800"),
+                    Command = new Command(async () =>
+                        await Application.Current.MainPage.Navigation.PushAsync(new Views.GestionDeProductos()))
+                }
+            };
+        }
+    }
+}
