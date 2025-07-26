1704e5965e9541160587b420d8047397e73ae7fe
mcalistriraga <mcalistri.freel@gmail.com>
2025-07-25 21:16:30 -0400

    Migrar MessagingCenter a WeakReferenceMessenger en gestión de categorías

---
+++ Messages/CategoriaMessages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace MauiAppGestorMovil.Messages
+{
+    public class CategoriaEditadaMessage : ValueChangedMessage<bool>
+    {
+        public CategoriaEditadaMessage(bool value) : base(value) { }
+    }
+
+    public class RecargarCategoriasMessage : ValueChangedMessage<bool>
+    {
+        public RecargarCategoriasMessage(bool value) : base(value) { }
+    }
+}
